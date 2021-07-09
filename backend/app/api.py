365e9f44f7d10d76a0062e07fa96eefa96ed735c
RAlanWright <wright.alan88@gmail.com>
2021-07-08 19:05:15 -0500

    Added react with chakra-ui

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-
 app = FastAPI()
 
 origins = [
@@ -17,6 +16,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
+
 
 # This is the base route
 @app.get("/", tags=["root"])
