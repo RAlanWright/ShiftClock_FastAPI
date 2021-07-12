49f233196dd9e483ae02957f211c8cd55a2d661b
RAlanWright <wright.alan88@gmail.com>
2021-07-12 16:51:17 -0500

    Troubleshooting and formatting

--- backend/app/api/server.py
+++ backend/app/api/server.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
 from app.api.routes import router as api_router
+from app.core import config, tasks
 
 
 def get_application():
-    app = FastAPI(title="docker-fastapi-test", version="1.0.0")
+    app = FastAPI(title=config.PROJECT_NAME, version=config.VERSION)
+
     app.add_middleware(
         CORSMiddleware,
         allow_origins=["*"],
@@ -13,6 +14,10 @@ def get_application():
         allow_methods=["*"],
         allow_headers=["*"],
     )
+
+    # Start and Stop scripts
+    app.add_event_handler("startup", tasks.create_start_app_handler(app))
+    app.add_event_handler("shutdown", tasks.create_stop_app_handler(app))
 
     app.include_router(api_router, prefix="/api")
 
