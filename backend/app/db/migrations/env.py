16f4453854f17a08ddbf1e6019eb40423164097d
RAlanWright <wright.alan88@gmail.com>
2021-07-13 00:08:06 -0500

    Added some tests

--- backend/app/db/migrations/env.py
+++ backend/app/db/migrations/env.py
@@ -1,13 +1,17 @@
 import pathlib
 import sys
+import os
+
 import alembic
-from sqlalchemy import engine_from_config, pool
+from sqlalchemy import create_engine, engine_from_config, pool
+from psycopg2 import DatabaseError
 
 from logging.config import fileConfig
 import logging
 
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[3]))
-from app.core.config import DATABASE_URL # noqa
+
+from app.core.config import DATABASE_URL, POSTGRES_DB  # noqa
 
 config = alembic.context.config
 
@@ -19,8 +23,19 @@ def run_migrations_online() -> None:
     """
     Run migrations in 'online' mode
     """
+
+    DB_URL = f"{DATABASE_URL}_test" if os.environ.get("TESTING") else str(DATABASE_URL)
+
+    # Testing config for migrations
+    if os.environ.get("TESTING"):
+        default_engine = create_engine(str(DATABASE_URL), isolation_level="AUTOCOMMIT")
+
+        with default_engine.connect() as default_conn:
+            default_conn.execute(f"DROP DATABASE IF EXISTS {POSTGRES_DB}_test")
+            default_conn.execute(f"CREATE DATABASE {POSTGRES_DB}_test")
+
     connectable = config.attributes.get("connection", None)
-    config.set_main_option("sqlalchemy.url", str(DATABASE_URL))
+    config.set_main_option("sqlalchemy.url", DB_URL)
 
     if connectable is None:
         connectable = engine_from_config(
@@ -43,6 +58,10 @@ def run_migrations_offline() -> None:
     """
     Run migrations in 'offline' mode.
     """
+
+    if os.environ.get("TESTING"):
+        raise DatabaseError("Running testing migrations offline isn't permitted at this time.")
+
     alembic.context.configure(url=str(DATABASE_URL))
 
     with alembic.context.begin_transaction():
--- backend/app/db/tasks.py
+++ backend/app/db/tasks.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from databases import Database
 from app.core.config import DATABASE_URL
@@ -7,6 +8,7 @@ logger = logging.getLogger(__name__)
 
 
 async def connect_to_db(app: FastAPI) -> None:
+    DB_URL = f"{DATABASE_URL}_test" if os.environ.get("TESTING") else DATABASE_URL
     database = Database(DATABASE_URL, min_size=2, max_size=10)
 
     try:
